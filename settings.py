272eb3c3250572550e18222dbb01372ba21e3d96
k4mkry <k4mkry@gmail.com>
2023-10-24 15:08:09 +0200

    updates

--- settings.py
+++ settings.py
@@ -8,29 +8,28 @@ class Style(ttk.Style):
     def __init__(self, root, **kw):
         ttk.Style.__init__(self, master=root, **kw)
         self.configure(
-            'warning.TLabel',
-            foreground='red4',
-            font=('Calibri', 12, 'bold'),
-            anchor='center'
+            "warning.TLabel",
+            foreground="red4",
+            font=("Calibri", 12, "bold"),
+            anchor="center",
         )
         self.configure(
-            'info.TLabel',
-            foreground='dark green',
-            font=('Calibri', 12, 'bold'),
-            anchor='center'
+            "info.TLabel",
+            foreground="dark green",
+            font=("Calibri", 12, "bold"),
+            anchor="center",
         )
 
 
 class Settings:
-
-    # Dev var
-    # DIRECTORY = os.path.join(os.getcwd(), 'WDT_path')
+    # Settings for development
+    DIRECTORY = os.path.join(os.getcwd(), "WDT_path")
     # DATABASE = os.path.join(os.getcwd(), 'emailer.db')
-    # Production var
-    DIRECTORY = '\\\\10.16.1.100\\Sroda\\Ksiegowosc\\Faktury do wysylki'
-    DATABASE = '\\\\10.16.1.103\\wysylka_faktur\\emailer.db'
-    date_now = datetime.now().strftime('%Y_%m_%d') + '-'
-    body = '''
+    # Settings for production
+    # DIRECTORY = "\\\\10.16.1.100\\Sroda\\Ksiegowosc\\Faktury do wysylki"
+    DATABASE = "\\\\10.16.1.103\\wysylka_faktur\\emailer.db"
+    date_now = datetime.now().strftime("%Y_%m_%d") + "-"
+    body = """
     <p>
     <span style="font-size:8px">[de] </span>
     Sehr geehrte Damen und Herren,
@@ -61,4 +60,4 @@ class Settings:
     Fax: +48 71 74 72 901<br>
     <a href="www.hmt-automotive.com">www.hmt-automotive.com</a>
     </i></small></p>
-    '''
+    """
